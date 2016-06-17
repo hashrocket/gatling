ff777bcdc6fbbe4222e929fca254374a298339ea
Micah Cooper <micah+github@mrmicahcooper.com>
2016-06-17 16:07:50 -0400

    User loads project

---
+++ lib/gatling/tasks/load.ex
@@ -0,0 +1,33 @@
+defmodule Mix.Tasks.Gatling.Load do
+  use Mix.Task
+
+  import Mix.Shell.IO, only: [prompt: 1, info: 1]
+
+  @deploy_dir System.user_home
+
+  def run([]) do
+    project_name = prompt("Please enter a project name:")
+    load(project_name)
+  end
+
+  def run([project_name]) do
+    load(project_name)
+  end
+
+  def load(project_name) do
+    dir = dir = dir(project_name) 
+    if File.exists?(dir) do
+      info ~s(-> #{dir} already exists)
+    else
+      File.mkdir_p!(dir)
+      System.cmd("git", ["init", dir])
+      info ~s(-> #{dir} created)
+    end
+  end
+
+  defp dir(project_name) do
+    project = String.strip(project_name)
+    @deploy_dir |> Path.join(project)
+  end
+
+end
