d666542039cda60b79116ea080f61b72d5709344
Micah Cooper <micah@mrmicahcooper.com>
2016-07-26 08:16:34 -0500

    Force mix compile

--- lib/gatling/tasks/deploy.ex
+++ lib/gatling/tasks/deploy.ex
@@ -48,8 +48,8 @@ defmodule Mix.Tasks.Gatling.Deploy do
   end
 
   def mix_compile(build_path) do
-    bash("mix", ["local", "rebar", "--force"], cd: build_path)
-    bash("mix", ["compile", "--no-archives-check", "--no-deps-check"], cd: build_path)
+    bash("mix", ["local.rebar", "--force"], cd: build_path)
+    bash("mix", ["compile", "--force"], cd: build_path)
   end
 
   def mix_release(build_path) do
