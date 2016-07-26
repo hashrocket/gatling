b0fd49620c5c1c7718646d2a93b53ac210b15abd
Micah Cooper <micah@mrmicahcooper.com>
2016-07-26 07:51:24 -0500

    Consolidate deployment functions

--- lib/gatling/tasks/upgrade.ex
+++ lib/gatling/tasks/upgrade.ex
@@ -1,7 +1,7 @@
 defmodule Mix.Tasks.Gatling.Upgrade do
   use Mix.Task
 
-  import Gatling.Bash, only: [bash: 3, bash: 2]
+  alias Mix.Tasks.Gatling.Deploy
 
   def run([]) do
     build_path = Mix.Shell.IO.prompt("Please enter the path to your project:")
@@ -14,63 +14,23 @@ defmodule Mix.Tasks.Gatling.Upgrade do
   end
 
   def upgrade(build_path) do
-    project       = Path.basename(build_path)
-    deploy_path   = Path.join([System.user_home, "deployments", project])
-    releases_path = Path.join([build_path, "rel", project, "releases"])
 
-    git_reset_hard(build_path)
-    mix_deps_get(build_path)
-    mix_compile(build_path)
-    mix_release(build_path)
-    copy_release_to_deploy(build_path, deploy_path)
-    upgrade_service(project)
+    Deploy.git_reset_hard(build_path)
+    Deploy.mix_deps_get(build_path)
+    Deploy.mix_compile(build_path)
+
+    project     = Path.basename(build_path)
+    version     = Deploy.mix_release(build_path)
+    deploy_path = Path.join([System.user_home, "deployments", project, version])
+
+    Deploy.make_deploy_dir(deploy_path)
+    Deploy.copy_release_to_deploy(build_path, deploy_path, version)
+
+    upgrade_service(project, version)
   end
 
-  def upgrade_service(project) do
+  def upgrade_service(project, version) do
     System.cmd("sudo", ["service", project, "upgrade", version])
   end
-
-  def git_reset_hard(build_path) do
-    bash("git", ["reset", "--hard"], cd: build_path)
-  end
-
-  def mix_deps_get(build_path) do
-    bash("mix", ["deps.get"], cd: build_path)
-  end
-
-  def mix_compile(build_path) do
-    bash("mix", ["compile"], cd: build_path)
-  end
-
-  def mix_release(build_path) do
-    bash("mix", ["release", "--no-confirm-missing"], cd: build_path)
-  end
-
-  def git_sha(build_path) do
-    System.cmd("git", ["rev-parse", "--short", "HEAD"], cd: build_path)
-    |> elem(0) |> String.trim()
-  end
-
-  def copy_release_to_deploy(build_path, deploy_path) do
-    release_from = release_from(build_path)
-    File.cp(release_from, deploy_path)
-  end
-
-  def release_from(build_path) do
-    project = Path.basename(build_path)
-    version = version(build_path)
-
-    [build_path, "rel", project, "releases", version, "#{project}.tar.gz"]
-    |> Path.join()
-  end
-
-  def version(build_path, project\\nil) do
-    project = Path.basename(build_path)
-    sha     = git_sha(build_path)
-    Path.join([build_path, "rel", project, "releases"])
-    |> File.ls!()
-    |> Enum.find(fn(path) -> Regex.match?(~r/#{sha}$/, path) end)
-  end
-
 
 end
