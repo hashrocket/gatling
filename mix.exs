d09f2b3b7d71f5b63904e653f78c1a79cad37875
Micah Cooper <micah@mrmicahcooper.com>
2016-07-26 22:58:40 -0500

    Setup for packaging

--- mix.exs
+++ mix.exs
@@ -2,30 +2,41 @@ defmodule Gatling.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :gatling,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :gatling,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      name: "Gatling",
+      source_url: "https://github.com/hashrocket/gatling",
+      package: package,
+      description: description,
+      deps: deps,
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+  defp description do
+    """
+    Enjoy heroku like deployments with Exrm and your own server.
+    """
+  end
+
+  defp package do
+    [
+      name: :gatling,
+      licenses: ["Apache 2.0"],
+      maintainers: ["Micah Cooper", "Hashrocket"],
+      links: %{
+        "GitHub" => "https://github.com/hashrocket/gatling",
+      }
+    ]
+  end
+
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
   end
