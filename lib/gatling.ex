b1be9743635465840cad6c95b65029c867fd6565
Micah Cooper <dev+mrmicahcooper@hashrocket.com>
2016-08-05 14:29:34 -0500

    Introduce Gatling.env

--- lib/gatling.ex
+++ lib/gatling.ex
@@ -1,2 +1,24 @@
 defmodule Gatling do
+  import Gatling.Utilities
+
+  def env(project, port\\nil) do
+    %{
+      :available_port       => port || available_port,
+      :build_dir            => build_dir(project),
+      :built_release_path   => built_release_path(project),
+      :deploy_dir           => deploy_dir(project),
+      :deploy_path          => deploy_path(project),
+      :domains              => domains(project),
+      :etc_dir              => etc_dir,
+      :etc_path             => etc_path(project),
+      :git_hook_path        => git_hook_path(project),
+      :nginx_available_path => nginx_available_path(project),
+      :nginx_dir            => nginx_dir,
+      :nginx_enabled_path   => nginx_enabled_path(project),
+      :upgrade_dir          => upgrade_dir(project),
+      :upgrade_path         => upgrade_path(project),
+      :version              => version(project),
+    }
+
+  end
 end
--- test/gatling_test.exs
+++ test/gatling_test.exs
@@ -1,4 +1,42 @@
 defmodule GatlingTest do
   use ExUnit.Case
-  doctest Gatling
+
+  alias Gatling.Utilities
+
+  setup do
+    build_dir = Gatling.Utilities.build_dir("sample_project")
+    File.mkdir_p(build_dir)
+    File.cp_r("test/sample_project", build_dir)
+
+    on_exit fn ->
+      File.rm_rf("test/root")
+      File.rm_rf("test/sample_project/rel")
+    end
+
+    IO.puts("")
+
+    :ok
+  end
+
+  test ".env" do
+    env = Gatling.env("sample_project", 4001)
+    assert env ==  %{
+     :available_port       => 4001,
+     :build_dir            => "/Users/dev/hashrocket/gatling/test/root/home/ubuntu/sample_project",
+     :built_release_path   => "/Users/dev/hashrocket/gatling/test/root/home/ubuntu/sample_project/rel/sample_project/releases/0.0.1470406670/sample_project.tar.gz",
+     :deploy_dir           => "/Users/dev/hashrocket/gatling/test/root/home/ubuntu/deployments/sample_project",
+     :deploy_path          => "/Users/dev/hashrocket/gatling/test/root/home/ubuntu/deployments/sample_project/sample_project.tar.gz",
+     :domains              => "www.example.com sample_project.hashrocket.com api.example.com",
+     :etc_dir              => "/Users/dev/hashrocket/gatling/test/root/etc/init.d",
+     :etc_path             => "/Users/dev/hashrocket/gatling/test/root/etc/init.d/sample_project",
+     :git_hook_path        => "/Users/dev/hashrocket/gatling/test/root/home/ubuntu/sample_project/.git/hooks/post-update",
+     :nginx_available_path => "/Users/dev/hashrocket/gatling/test/root/etc/nginx/sites-available/sample_project",
+     :nginx_dir            => "/Users/dev/hashrocket/gatling/test/root/etc/nginx",
+     :nginx_enabled_path   => "/Users/dev/hashrocket/gatling/test/root/etc/nginx/sites-enabled/sample_project",
+     :upgrade_dir          => "/Users/dev/hashrocket/gatling/test/root/home/ubuntu/deployments/sample_project/releases/0.0.1470406670",
+     :upgrade_path         => "/Users/dev/hashrocket/gatling/test/root/home/ubuntu/deployments/sample_project/releases/0.0.1470406670/sample_project.tar.gz",
+     :version              => "0.0.1470406670"
+   }
+  end
+
 end
